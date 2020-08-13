c7371e124cee90b6bb9577bf750a990ddd88875b
Ray Wang <ray.hackmylife@gmail.com>
2020-08-13 21:55:01 +0800

    remove unnecessary code

--- init.el
+++ init.el
@@ -3,16 +3,7 @@
      ("http" . "localhost:41091")
      ("https" . "localhost:41091")))
 
-(add-to-list 'load-path (concat user-emacs-directory "lisp"))
-(defconst *is-a-mac* (eq system-type 'darwin))
-
 (setq spacemacs-start-directory (concat user-emacs-directory "spacemacs/"))
 (load-file (concat spacemacs-start-directory "init.el"))
 
-;; (require 'init-package)
-;; (require 'init-editor-enhancement)
-;; (require 'init-spacemacs)
-;; (require 'init-keys)
-;; (require 'init-window)
-;; (require 'init-postload-local)
 (provide 'init)
