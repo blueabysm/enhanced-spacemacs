9ad759ef60cd957add1c1015c9845aa5fdc62277
Ray Wang <ray.hackmylife@gmail.com>
2020-08-13 21:35:29 +0800

    More layers

---
+++ layers/fav-keys/keybindings.el
@@ -0,0 +1,6 @@
+(global-set-key (kbd "M-`") 'other-window)
+(global-set-key (kbd "C-x C-c")
+                #'(lambda ()
+                    (interactive)
+                    (if (y-or-n-p "Exit? ")
+                        (save-buffers-kill-terminal))))
