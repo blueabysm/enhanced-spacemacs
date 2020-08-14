9ad2086ea541fd383c48147af63c0c110c201d79
Ray Wang <ray.hackmylife@gmail.com>
2020-08-14 16:32:07 +0800

    remove proxy and change error message

--- init.el
+++ init.el
@@ -1,12 +1,7 @@
-(setq url-proxy-services
-   '(("no_proxy" . "^\\(localhost\\|10\\..*\\|192\\.168\\..*\\)")
-     ("http" . "localhost:41091")
-     ("https" . "localhost:41091")))
-
 (setq spacemacs-start-directory (concat user-emacs-directory "spacemacs/"))
 (let ((spacemacs-init-file (concat spacemacs-start-directory "init.el")))
   (if (file-exists-p spacemacs-init-file)
       (load-file spacemacs-init-file)
-    (error "Failed to load file %s. Have you pulled from syl20bnr/spacemacs?" spacemacs-init-file)))
+    (error "Failed to load file %s. Have you done `submodule update' operation?" spacemacs-init-file)))
 
 (provide 'init)
