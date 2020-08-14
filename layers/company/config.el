6c321e9e5a0a51221ba2b0763d60fbdc591b3776
Ray Wang <ray.hackmylife@gmail.com>
2020-08-14 14:55:42 +0800

    enable company-mode in specified major-modes

--- layers/company/config.el
+++ layers/company/config.el
@@ -3,3 +3,12 @@
 (setq auto-completion-enable-snippets-in-popup nil)
 ;; Do not enable global company mode. The following shows how to enable global-company-mode, but I recommend you use company-mode per major-mode.
 ;; (add-hook 'after-init-hook 'global-company-mode)
+
+(dolist (hook '(emacs-lisp-mode-hook
+                 js2-mode-hook
+                 js-mode-hook
+                 go-mode-hook
+                 markdown-mode-hook))
+  (add-hook hook #'(lambda ()
+                     (company-mode)
+                     (company))))
