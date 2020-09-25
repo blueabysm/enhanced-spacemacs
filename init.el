c78610c44d3e41474e46955a3e9a6a910831f7d4
Ray Wang <ray.hackmylife@gmail.com>
2020-09-25 16:51:23 +0800

    + elegant-emacs

--- init.el
+++ init.el
@@ -1,7 +1,13 @@
-(setq spacemacs-start-directory (concat user-emacs-directory "spacemacs/"))
-(let ((spacemacs-init-file (concat spacemacs-start-directory "init.el")))
-  (if (file-exists-p spacemacs-init-file)
-      (load-file spacemacs-init-file)
-    (error "Failed to load file %s. Have you done `submodule update' operation?" spacemacs-init-file)))
+(defconst *emacs-use* 'spacemacs)
+
+(cond ((equal *emacs-use* 'spacemacs)
+       (setq spacemacs-start-directory (concat user-emacs-directory "spacemacs/"))
+       (let ((spacemacs-init-file (concat spacemacs-start-directory "init.el")))
+         (if (file-exists-p spacemacs-init-file)
+             (load-file spacemacs-init-file)
+           (error "Failed to load file %s. Have you done `submodule update' operation?" spacemacs-init-file))))
+      (t (add-to-list 'load-path (concat user-emacs-directory "elegant-emacs"))
+         (require 'elegance)
+         (require 'sanity)))
 
 (provide 'init)
