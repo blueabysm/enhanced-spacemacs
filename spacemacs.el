a2a1207ade34f546d48c9731186e0d684bee2312
Ray Wang <ray.hackmylife@gmail.com>
2020-09-27 15:18:22 +0800

    + latex

--- spacemacs.el
+++ spacemacs.el
@@ -78,6 +78,7 @@ This function should only modify configuration layer settings."
      syntax-checking
      git
      vue
+     latex
      (version-control :variables
                       version-control-diff-side 'left
                       version-control-diff-tool 'diff-hl
